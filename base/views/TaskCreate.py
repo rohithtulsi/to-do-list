c505012037b6c1a598fc9bfb5cdd6fb173333145
rohithtulsi <rohithtm01@gmail.com>
2022-09-29 00:54:32 +0530

    users app

--- base/views/TaskCreate.py
+++ base/views/TaskCreate.py
@@ -1,14 +1,14 @@
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from base.models import Task
+from base.models import Task, List
 
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = ['title', 'description', 'complete', 'tags']
     success_url = reverse_lazy('tasks')
-
+    
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super(TaskCreate, self).form_valid(form)
--- base/views/TaskList.py
+++ base/views/TaskList.py
@@ -1,7 +1,7 @@
 
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from base.models import Task
+from base.models import Task, List
 
 
 
