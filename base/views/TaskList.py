e08b0b504655a1f4cd831fc264d14232ea8884e0
rohithtulsi <rohithtm01@gmail.com>
2022-10-02 22:35:18 +0530

    urls mapped

--- base/views/TaskList.py
+++ base/views/TaskList.py
@@ -12,6 +12,7 @@ class TaskList(LoginRequiredMixin, ListView):
         context['tasks'] = context['tasks'].filter(created_by=self.request.user, listno=self.kwargs['listid'])
         context['count'] = context['tasks'].filter(complete=False).count()
         context['list'] = List.objects.get(id=self.kwargs['listid'])
+        context['listname'] = List.objects.get(id=self.kwargs['listid'])
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
             context['tasks'] = context['tasks'].filter(title__startswith=search_input)
