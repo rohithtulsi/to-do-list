14885147fe7403fbf9d5a9cb9596a92853bb5c03
rohithtulsi <rohithtm01@gmail.com>
2022-10-05 00:21:52 +0530

    tags as buttons

--- users/forms.py
+++ users/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Create your forms here.
 
 class NewUserForm(UserCreationForm):
-
+    
 	class Meta:
 		model = User
 		fields = ("username", "password1", "password2")
