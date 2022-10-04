dec5456417689316f074fcc92f2b757075078542
rohithtulsi <rohithtm01@gmail.com>
2022-10-04 14:53:09 +0530

    Floating button

--- users/views.py
+++ users/views.py
@@ -22,13 +22,13 @@ class RegisterPage(FormView):
     redirect_authenticated_user = True
     success_url = reverse_lazy('lists')
     
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('lists')
+        return super(RegisterPage, self).get(args, **kwargs)
+
     def form_valid(self, form):
         user = form.save()
         if user is not None:
             login(self.request, user)
         return super(RegisterPage, self).form_valid(form)
-
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            return redirect('lists')
-        return super(RegisterPage, self).get(args, **kwargs)
