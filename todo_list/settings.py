7d62aeb2daaff177b1b608ed8e26260145ec6318
rohithtulsi <rohithtm01@gmail.com>
2022-10-11 01:44:55 +0530

    errors messages

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -124,7 +124,10 @@ LOGIN_URL = 'login'
 
 STATIC_URL = '/static/'
 
-
+STATICFILES_DIRS = [
+     BASE_DIR / "static",
+     '/base/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
