094131206e637c9913d87716c3a7b8d184ca68bd
rohithtulsi <rohithtm01@gmail.com>
2022-10-20 01:00:50 +0530

    final validations

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from tkinter import Widget
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ValidationError
 
 class NewUserForm(UserCreationForm):
 	class Meta:
@@ -14,3 +14,11 @@ class NewUserForm(UserCreationForm):
 		if commit:
 			user.save()
 		return user
+
+	def validate(self, password, user=None):
+		if len(password) < self.min_length:
+			raise ValidationError(
+                _("This password must contain at least %(min_length)d characters."),
+                code='password_too_short',
+                params={'min_length': self.min_length},
+            )
