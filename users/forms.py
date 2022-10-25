f39ddc4a5982e561209e461a0191f685d86e0c0a
rohithtulsi <rohithtm01@gmail.com>
2022-10-25 23:36:31 +0530

    forgot password style

--- users/forms.py
+++ users/forms.py
@@ -24,12 +24,26 @@ class NewUserForm(UserCreationForm):
 			user.save()
 		return user
 
+	def clean_email(self):
+        # Get the email
+		email = self.cleaned_data.get('email')
+
+        # Check to see if any users already exist with this email as a username.
+		try:
+			match = User.objects.get(email=email)
+		except User.DoesNotExist:
+            # Unable to find a user, this is fine
+			return email
+
+        # A user was found with this as a username, raise an error.
+		raise forms.ValidationError('This email address is already in use.')
 
 
-	def clean(self):	
+	def clean(self):
+			
 		cleaned_data = self.cleaned_data
 		username = cleaned_data.get('username')
-		
+
 
 		if username:
 			if len(username)<4:
